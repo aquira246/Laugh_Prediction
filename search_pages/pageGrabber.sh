31b733c639eb80bc82184065974f0f75832d44ca
aquira246 <munchkin9955@yahoo.com>
2015-11-01 10:49:50 -0800

    updated git ignore. now it adds the data I am currently using to test it as well as the sh files

---
+++ search_pages/pageGrabber.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+wget https://www.ted.com/talks?page=1&sort=funny
+wget https://www.ted.com/talks?page=2&sort=funny
+wget https://www.ted.com/talks?page=3&sort=funny
+wget https://www.ted.com/talks?page=4&sort=funny
+wget https://www.ted.com/talks?page=5&sort=funny
+wget https://www.ted.com/talks?page=6&sort=funny
+wget https://www.ted.com/talks?page=7&sort=funny
+wget https://www.ted.com/talks?page=8&sort=funny
+wget https://www.ted.com/talks?page=9&sort=funny
+wget https://www.ted.com/talks?page=10&sort=funny
+wget https://www.ted.com/talks?page=11&sort=funny
+wget https://www.ted.com/talks?page=12&sort=funny
+wget https://www.ted.com/talks?page=13&sort=funny
+wget https://www.ted.com/talks?page=14&sort=funny
+wget https://www.ted.com/talks?page=15&sort=funny
+wget https://www.ted.com/talks?page=16&sort=funny
+wget https://www.ted.com/talks?page=17&sort=funny
+wget https://www.ted.com/talks?page=18&sort=funny
+wget https://www.ted.com/talks?page=19&sort=funny
+wget https://www.ted.com/talks?page=20&sort=funny
+wget https://www.ted.com/talks?page=21&sort=funny
+wget https://www.ted.com/talks?page=22&sort=funny
+wget https://www.ted.com/talks?page=23&sort=funny
+wget https://www.ted.com/talks?page=24&sort=funny
